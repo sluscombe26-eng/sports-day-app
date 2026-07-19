c45cc1f1ece00b3dcf15211cd616180e981f2d98
sluscombe26-eng <sluscombe26@gmail.com>
2026-07-19 16:49:06 +0100

    Update TeamPicker.tsx

--- components/TeamPicker.tsx
+++ components/TeamPicker.tsx
@@ -3,9 +3,15 @@ type Team = {
   team_name: string;
 };
 
+type Member = {
+  team_id: string;
+  participant_name: string;
+};
+
 type Props = {
   eventName: string;
   teams: Team[];
+  members: Member[];
   selectedTeam: string;
   onSelect: (teamId: string) => void;
 };
@@ -13,6 +19,7 @@ type Props = {
 export default function TeamPicker({
   eventName,
   teams,
+  members,
   selectedTeam,
   onSelect,
 }: Props) {
@@ -27,25 +34,48 @@ export default function TeamPicker({
     >
       <h4>Choose Team for {eventName}</h4>
 
-      {teams.map((team) => (
-        <label
-          key={team.id}
-          style={{
-            display: "block",
-            marginBottom: "8px",
-          }}
-        >
-          <input
-            type="radio"
-            name={eventName}
-            checked={selectedTeam === team.id}
-            onChange={() => onSelect(team.id)}
-          />
+      {teams.map((team) => {
+        const teamMembers = members.filter(
+          (m) => m.team_id === team.id
+        );
 
-          {" "}
-          {team.team_name}
-        </label>
-      ))}
+        return (
+          <div
+            key={team.id}
+            style={{
+              border: "1px solid #ddd",
+              marginBottom: "12px",
+              padding: "12px",
+              borderRadius: "8px",
+              background: "white",
+            }}
+          >
+            <label>
+              <input
+                type="radio"
+                name={eventName}
+                checked={selectedTeam === team.id}
+                onChange={() => onSelect(team.id)}
+              />
+
+              {" "}
+              <strong>{team.team_name}</strong>
+            </label>
+
+            <div style={{ marginTop: "8px" }}>
+              {teamMembers.length} members
+            </div>
+
+            <ul>
+              {teamMembers.map((member) => (
+                <li key={member.participant_name}>
+                  {member.participant_name}
+                </li>
+              ))}
+            </ul>
+          </div>
+        );
+      })}
     </div>
   );
 }
