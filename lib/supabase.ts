681a568128b09841d6914f8b8353c9a5afd1cbe9
sluscombe26-eng <sluscombe26@gmail.com>
2026-07-19 15:24:32 +0100

    Connect Supabase

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,2 +1,6 @@
-export const supabaseUrl =
-  "https://segurwezezhfljxorynu.supabase.co";
+import { createClient } from "@supabase/supabase-js";
+
+export const supabase = createClient(
+  "https://segurwezezhfljxorynu.supabase.co",
+  "sb_publishable_WqWEgHJu5rsSP5ZCkiRR3w_FboB2sRt"
+);
