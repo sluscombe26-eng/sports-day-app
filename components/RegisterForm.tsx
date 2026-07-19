a5fd7d726191ef379188a755848ed2bc75aba589
sluscombe26-eng <sluscombe26@gmail.com>
2026-07-19 15:15:11 +0100

    Create registration form

---
+++ components/RegisterForm.tsx
@@ -0,0 +1,23 @@
+export default function RegisterForm() {
+  return (
+    <div>
+      <h2>Register</h2>
+
+      <input placeholder="Full Name" />
+
+      <br />
+
+      <input placeholder="Email Address" />
+
+      <br />
+
+      <input placeholder="Mobile Number" />
+
+      <br />
+
+      <button>
+        Register
+      </button>
+    </div>
+  );
+}
