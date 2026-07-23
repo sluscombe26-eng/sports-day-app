597100edfbb7525a0131448cb554f9b833147207
sluscombe26-eng <sluscombe26@gmail.com>
2026-07-23 11:18:34 +0100

    update checkin page - live

--- app/admin/checkin/page.tsx
+++ app/admin/checkin/page.tsx
@@ -1,5 +1,7 @@
 import { supabase } from "../../../lib/supabase";
 
+export const dynamic = "force-dynamic";
+
 export default async function CheckInPage() {
   const participants = await supabase
     .from("participants")
